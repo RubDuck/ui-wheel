781d2b4599c5137a00a70fdfa35691ceda895e28
hcl <2082000367@qq.com>
2019-04-15 20:32:27 +0800

    goods statue

--- ui-button/src/button.js
+++ ui-button/src/button.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import button from './button.vue'
-<<<<<<< HEAD
 import  icon from  './icon.vue'
-=======
->>>>>>> 827c75e4c2288f65c5fc6377cd6d0121a882aa76
-
-
 Vue.component('g-button',button)
 Vue.component('g-icon',icon)
 
