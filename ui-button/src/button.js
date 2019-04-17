ebd5a0b5a76693f9a8c9b3ea5972fe5834adaafb
hcl <2082000367@qq.com>
2019-04-17 11:18:29 +0800

    BDD chai 3个测试

--- ui-button/src/button.js
+++ ui-button/src/button.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import button from './button.vue'
 import  icon from  './icon.vue'
 import gbutton from './g-button.vue'
+import { builtinModules } from 'module';
 Vue.component('g-button',button)
 Vue.component('g-icon',icon)
 Vue.component('g-button-group',gbutton)
@@ -11,5 +12,67 @@ new  Vue({
     data:{
         loading:true
     }
-
 })
+
+//单元测试1
+
+import chai from 'chai'
+const expect= chai.expect
+{
+const Constructor=Vue.extend(button)
+const vm =new Constructor({
+    propsData:{
+        icon:'setting'
+    }
+})
+
+vm.$mount('#test')
+let useElement=vm.$el.querySelector('use')
+expect(useElement.getAttribute('xlink:href')).to.eq('#icon-setting')
+vm.$el.remove()
+vm.$destroy()
+
+}
+
+
+//单元测试2
+
+{
+    const Constructor=Vue.extend(button)
+    const vm =new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true
+        }
+    })
+    
+    vm.$mount()
+    let useElement=vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#icon-loading') 
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+
+
+
+//单元测试3
+
+{
+    const div=document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor=Vue.extend(button)
+    const vm =new Constructor({
+        propsData:{
+            icon:'setting',
+        }
+    })
+    
+    vm.$mount(div)
+    let useElement=vm.$el.querySelector('svg')
+    let {order}=window.getComputedStyle(useElement)
+    expect(order).to.eq('-1')  
+    vm.$el.remove()
+    vm.$destroy()
+}
+
